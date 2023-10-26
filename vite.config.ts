c7e248f447753676e9490d23f069d9a1f9976d83
snct-ukai <r02i08@ed.cc.suzuka-ct.ac.jp>
2023-10-26 22:58:52 +0900

    update: pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,39 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import {VitePWA, VitePWAOptions} from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
+const pwaConfig : VitePWAOptions = {
+  disable: false,
+  includeManifestIcons: false,
+  injectManifest: undefined,
+  injectRegister: undefined,
+  minify: false,
+  workbox: undefined,
+  registerType: 'autoUpdate',
+  includeAssets: ['icon.png'],
+  manifest:{
+    theme_color: "#FFFFFF",
+    background_color: "#FFFFFF",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    name: "webtoys",
+    short_name: "webtoys",
+    icons: [
+      {
+        src: "/icon.png",
+        sizes: "256x256",
+        type: "image/png",
+      }
+    ]
+  },
+  devOptions:{
+    enabled: true,
+  }
+}
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    VitePWA(pwaConfig),
+    react()],
 })
