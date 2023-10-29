78035cf09effd77722df19c7fae9aa341430b941
snct-ukai <r02i08@ed.cc.suzuka-ct.ac.jp>
2023-10-30 00:50:04 +0900

    update: validate at BaseConversion

--- src/pages/BaseConversion.tsx
+++ src/pages/BaseConversion.tsx
@@ -3,45 +3,38 @@ import {TextField} from "@mui/material";
 import style from "./BaseConversion.module.scss";
 
 const BaseConversion = () => {
-  const [binary, setBinary] = useState("");
-  const [octal, setOctal] = useState("");
-  const [decimal, setDecimal] = useState("");
-  const [hexadecimal, setHexadecimal] = useState("");
+  const [number, setNumber] = useState("");
 
   const handleBinaryChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setBinary(event.target.value);
-    setOctal(parseInt(event.target.value, 2).toString(8));
-    setDecimal(parseInt(event.target.value, 2).toString(10));
-    setHexadecimal(parseInt(event.target.value, 2).toString(16));
+    const value = event.target.value;
+    const binaryValue = value.replace(/[^0-1]/g, "");
+    setNumber(parseInt(binaryValue, 2).toString(10));
   }
 
   const handleOctalChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setBinary(parseInt(event.target.value, 8).toString(2));
-    setOctal(event.target.value);
-    setDecimal(parseInt(event.target.value, 8).toString(10));
-    setHexadecimal(parseInt(event.target.value, 8).toString(16));
+    const value = event.target.value;
+    const octalValue = value.replace(/[^0-7]/g, "");
+    setNumber(parseInt(octalValue, 8).toString(10));
   }
 
   const handleDecimalChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setBinary(parseInt(event.target.value, 10).toString(2));
-    setOctal(parseInt(event.target.value, 10).toString(8));
-    setDecimal(event.target.value);
-    setHexadecimal(parseInt(event.target.value, 10).toString(16));
+    const value = event.target.value;
+    const decimalValue = value.replace(/[^0-9]/g, "");
+    setNumber(decimalValue);
   }
 
   const handleHexadecimalChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setBinary(parseInt(event.target.value, 16).toString(2));
-    setOctal(parseInt(event.target.value, 16).toString(8));
-    setDecimal(parseInt(event.target.value, 16).toString(10));
-    setHexadecimal(event.target.value);
+    const value = event.target.value;
+    const hexadecimalValue = value.replace(/[^0-9a-fA-F]/g, "");
+    setNumber(parseInt(hexadecimalValue, 16).toString(10));
   }
 
   return (
     <div className={style.container}>
-      <TextField className={style.item} label="Binary" value={binary} onChange={handleBinaryChange} size="small"/>
-      <TextField className={style.item} label="Octal" value={octal} onChange={handleOctalChange} size="small"/>
-      <TextField className={style.item} label="Decimal" value={decimal} onChange={handleDecimalChange} size="small"/>
-      <TextField className={style.item} label="Hexadecimal" value={hexadecimal} onChange={handleHexadecimalChange} size="small"/>
+      <TextField className={style.item} label="Binary" value={parseInt(number).toString(2)} onChange={handleBinaryChange} size="small"/>
+      <TextField className={style.item} label="Octal" value={parseInt(number).toString(8)} onChange={handleOctalChange} size="small"/>
+      <TextField className={style.item} label="Decimal" value={number} onChange={handleDecimalChange} size="small"/>
+      <TextField className={style.item} label="Hexadecimal" value={parseInt(number).toString(16)} onChange={handleHexadecimalChange} size="small"/>
     </div>
   );
 }
